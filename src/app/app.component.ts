dc132da1c622789fb993e20c4955df5f033be19f
unknown <sumair_ahmed4@yahoo.co.in>
2018-11-15 22:38:24 +1100

    Updated recipe list component to use recipe item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipes';
+
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
+
 }
