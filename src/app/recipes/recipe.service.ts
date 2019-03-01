38c53343acca908c0e69041786c0dfbc2ce0c796
Sumair Syed <sumair@rockagency.com.au>
2019-03-01 17:04:26 +1100

    Added event emitter for adding ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core'
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
 	recipeSelected = new EventEmitter<Recipe>()
 
 	private recipes: Recipe[] = [
-		new Recipe('Test recipe', 'This is a simple beef recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/480px-Recipe_logo.jpeg'),
-		new Recipe('Another test recipe', 'This is a simple chicken recipe', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/480px-Recipe_logo.jpeg')
+		new Recipe(
+			'Test recipe', 
+			'This is a simple beef recipe', 
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/480px-Recipe_logo.jpeg',
+			[
+				new Ingredient('Meat', 1),
+				new Ingredient('French Fries', 20),
+			]
+		),
+		new Recipe(
+			'Another test recipe', 
+			'This is a simple chicken recipe', 
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/480px-Recipe_logo.jpeg',
+			[
+				new Ingredient('Buns', 2),
+				new Ingredient('Meat', 1),
+			]
+		)
 	]
 
 	getRecipes() {
