a475cd7a4a400149ffb3dd78571712695ee54deb
Sumair Syed <sumair@rockagency.com.au>
2019-03-14 10:24:12 +1100

    Added delete functionality for shopping list items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,4 +53,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false
     form.reset()
   }
+
+  onClear() {
+    this.slForm.reset()
+    this.editMode = false
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
+  }
 }
