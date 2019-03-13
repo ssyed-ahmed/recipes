01b19d30a043c2c0a18c3f06ae34a8797062e9e1
Sumair Syed <sumair@rockagency.com.au>
2019-03-13 12:43:56 +1100

    Updated shopping list tmplate to use template driven forms

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,8 @@
 import { 
   Component, 
-  OnInit, 
-  ViewChild, 
-  ElementRef
+  OnInit
 } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shoppinglist.service';
 
@@ -14,18 +13,14 @@ import { ShoppingListService } from '../shoppinglist.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameInputRef: ElementRef
-  @ViewChild('amountInput') amountInputRef: ElementRef
-
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value
-    const ingAmount = this.amountInputRef.nativeElement.value
-    const newIngredient = new Ingredient(ingName, ingAmount)
+  onAddItem(form: NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount)
     this.slService.addIngredient(newIngredient)
   }
 }
