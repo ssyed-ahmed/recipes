8f6c9c5e0a315cb6ecec3dfc578417749c2f08d4
Sumair Syed <sumair@rockagency.com.au>
2019-03-18 15:14:40 +1100

    Added formArray for recipe ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -29,17 +29,29 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = ''
     let recipeImagePath = ''
     let recipeDescription = ''
+    let recipeIngredients = new FormArray([])
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id)
       recipeName = recipe.name
       recipeImagePath = recipe.imagePath
       recipeDescription = recipe.description
+      if (recipe.ingredients) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = this.fBuilder.group({
       'name': [recipeName],
       'imagePath': [recipeImagePath],
-      'description': [recipeDescription]
+      'description': [recipeDescription],
+      'ingredients': recipeIngredients
     })
   }
 
